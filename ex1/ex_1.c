9332c53dc7ad00ebd345f9f473450527afbce83f
Ben Tau <62897867+bentau19@users.noreply.github.com>
2024-01-17 21:32:35 +0200

    Final job done
    I edited it and added comments!

--- ex1/ex_1.c
+++ ex1/ex_1.c
@@ -3,34 +3,31 @@
 int main()
 {
     //ex1
+    // the code takes 2 4 bits binary strings and make xor action on it
     printf("Exercise 1:\n");
     printf("Please enter 2 binary strings with 4 digits:\n");
-
     char bin11, bin12, bin13, bin14;
     printf("Binary 1: ");
     scanf(" %c %c %c %c", &bin11, &bin12, &bin13, &bin14);
-
     char bin21, bin22, bin23, bin24;
     printf("Binary 2: ");
     scanf(" %c %c %c %c", &bin21, &bin22, &bin23, &bin24);
-
     printf("\n");
     printf("%c%c%c%c\n", bin11, bin12, bin13, bin14);
     printf("^\n");
     printf("%c%c%c%c\n", bin21, bin22, bin23, bin24);
     printf("--------\n");
-
     bin11 = ((bin11 - '0') ^ (bin21 - '0')) + '0';
     bin12 = ((bin12 - '0') ^ (bin22 - '0')) + '0';
     bin13 = ((bin13 - '0') ^ (bin23 - '0')) + '0';
     bin14 = ((bin14 - '0') ^ (bin24 - '0')) + '0';
-
     printf("%c%c%c%c\n", bin11, bin12, bin13, bin14);
     printf("\n");
 
 
 
     //ex2
+    //The code takes 2 hex numbers make + action on them and print the binary value of the last number.
     printf("Exercise 2:\n");
     printf("Enter 2 hexadecimal numbers:\n");
     printf("Hex 1: ");
@@ -40,14 +37,17 @@ int main()
     scanf(" %X",&hex2);
     int res2 = hex1+hex2;
     printf("\n%X + %X = %X\n",hex1,hex2,res2);
-    int num21 = res2%2;
-    int num22 = (res2/2)%2;
-    int num23 = ((res2/2)/2)%2;
-    int num24 = (((res2/2)/2)/2)%2;
+    const int binDivider = 2;
+    int num21 = res2%binDivider;
+    int num22 = (res2/binDivider)%binDivider;
+    int num23 = ((res2/binDivider)/binDivider)%binDivider;
+    int num24 = (((res2/binDivider)/binDivider)/binDivider)%binDivider;
     printf("The last 4 binary digits of the sum are %d%d%d%d",num24,num23,num22,num21);
     printf("\n\n");
 
     //ex3
+    //The code takes num between 2-9 then takes 5 digit num and returns the decimal value
+    //of the second num in the base of the first num
     printf("Exercise 3:\n");
     printf("Choose a base between 2 to 9: ");
     int base3;
@@ -62,6 +62,7 @@ int main()
     printf("\n\n");
 
     //ex4
+    //The code take a number and a value of a bit and returns the value of that bit.
     printf("Exercise 4:\n");
     printf("Enter a number: ");
     int num41;
@@ -69,7 +70,7 @@ int main()
     printf("I want to know the value of bit number: ");
     int bitIdx;
     scanf(" %d", &bitIdx);
-    int bitVal = (num41 & (1 << (bitIdx - 1))) != 0;
+    int bitVal = (num41 >> (bitIdx -1))&1;
     printf("\nThe value of the %d bit in %d is %d\n", bitIdx, num41, bitVal);
     printf("Congrats! You've found the philosopher's stone!");
 
